50f10a292df734fc1a0e2e20f8262af4f6006b1d
grahamcrowell <graham.crowell@gmail.com>
2015-02-08 20:49:50 -0800

    data_structs.py, Portfolio class using functional techniques, clean CSV backups

--- git_commands.sh
+++ git_commands.sh
@@ -1,3 +1,4 @@
+!bin
 commit_str="data_structs.py, Portfolio class using functional techniques, clean CSV backups"
 
 cwd=echo pwd
@@ -5,5 +6,5 @@ git init
 git status
 git add "$cwd"
 git commit -m "$commit_str"
-git remote add origin https://github.com/grahamcrowell/pyfolio.git
+# git remote add origin https://github.com/grahamcrowell/pyfolio.git
 git push -u origin master
